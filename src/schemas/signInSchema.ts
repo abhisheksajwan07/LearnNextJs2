30c1b63a2c9b9639c61c82da78e5f440e465de3e
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-08-10 19:24:41 +0530

    fix: change email to identifer

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -1,5 +1,9 @@
 import { z } from "zod";
 export const signInSchema = z.object({
-  email: z.string().email("Invalid email"),
+  // email: z.string().email("Invalid email"),
+
+  //   Sometimes apps allow login using either username or email.
+  // Using a generic field name like identifier lets the backend handle whether it's an email or username.
+  identifier: z.string(),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
