e57d9502a87e421dfcd3c262d08ba57561e547aa
abhishek sajwan <abhisheksajwan2004@gmail.com>
2026-03-05 15:16:53 +0530

    nextjs new params

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -7,22 +7,15 @@ import { authOptions } from "../../auth/[...nextauth]/options";
 
 export async function DELETE(
   request: Request,
-  {
-    params,
-  }: {
-    params: { messageid: string };
-  }
+  { params }: { params: Promise<{ messageid: string }> }
 ) {
-  const messageId = params.messageid;
+  const { messageid: messageId } = await params;
   await dbConnect();
   const session = await getServerSession(authOptions);
   const user: User = session?.user as User;
   if (!session || !session.user) {
     return Response.json(
-      {
-        success: false,
-        message: "not authenticated",
-      },
+      { success: false, message: "not authenticated" },
       { status: 401 }
     );
   }
@@ -34,33 +27,20 @@ export async function DELETE(
     );
     if (updateResult.modifiedCount == 0) {
       return Response.json(
-        {
-          success: false,
-          message: "Message not found or already delete",
-        },
+        { success: false, message: "Message not found or already deleted" },
         { status: 404 }
       );
     }
 
     return Response.json(
-      {
-        success: true,
-        messages: "message deleted",
-      },
-      {
-        status: 200,
-      }
+      { success: true, messages: "message deleted" },
+      { status: 200 }
     );
   } catch (err) {
     console.log("an unexpected error occured in delete message route:", err);
     return Response.json(
-      {
-        success: false,
-        message: "error deleting message",
-      },
-      {
-        status: 500,
-      }
+      { success: false, message: "error deleting message" },
+      { status: 500 }
     );
   }
 }
