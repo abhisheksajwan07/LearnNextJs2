7e0e27d699e179bb22cf4c8972969f2c6dbdc98b
abhishek sajwan <abhisheksajwan2004@gmail.com>
2026-03-05 15:21:31 +0530

    fixed wrong zod import

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,10 +1,9 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-
 import bcrypt from "bcryptjs";
 import dbConnect from "@/lib/dbConnect";
 import { User } from "@/model/User";
-import { email } from "zod";
+
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
